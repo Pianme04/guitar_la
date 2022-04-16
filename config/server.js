137f76453e9a81bc17f043d34155714748100a53
Pianme04 <rdzhector.guillermo@hotmail.com>
2022-04-16 00:30:54 -0500

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c966c469b076505ed0bd45196aff3ffe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'da033953-6a09-4517-af08-dbf90cd0df32'
+};
